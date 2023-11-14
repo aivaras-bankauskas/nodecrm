7319a2f9a15214e002086cb0c441bebc6805e011
aivaras <aivarasbankauskas@gmail.com>
2023-11-14 17:07:43 +0200

    improve dev environment

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
+dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+export default app;
