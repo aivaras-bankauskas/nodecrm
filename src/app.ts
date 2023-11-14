a0922ee0c4e8e6d2f9949057947b6e9e3146bef2
aivaras <aivarasbankauskas@gmail.com>
2023-11-14 18:10:30 +0200

    database.ts: add connection to mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './config/database';
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+
+connectDB();
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
