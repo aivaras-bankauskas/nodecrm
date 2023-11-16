123d45fff81f3bdcc93383f54ac8e655bc45b16f
aivaras <aivarasbankauskas@gmail.com>
2023-11-16 11:49:34 +0200

    api.ts: add auth routes and update user routes

--- src/api/routes/api.ts
+++ src/api/routes/api.ts
@@ -1,6 +1,14 @@
 import router from '../../utils/handlers/routeResourceHandler';
 import userController from '../controllers/userController';
+import loginController from '../controllers/authentication/loginController';
+import registrationController from '../controllers/authentication/registrationController';
 
-router.resource('/users', userController);
+router.post('/login', loginController.login);
+router.post('/register', registrationController.register);
+
+router.get('/users', userController.index);
+router.get('/users/:id', userController.show);
+router.put('/users/:id', userController.update);
+router.delete('/users/:id', userController.destroy);
 
 export default router;
