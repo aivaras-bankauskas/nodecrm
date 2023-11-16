7dccbec93e6a611e5bd2db67a67b44a5669c1c81
aivaras <aivarasbankauskas@gmail.com>
2023-11-16 16:48:20 +0200

    registrationController.ts: optimize code

--- src/api/controllers/authentication/registrationController.ts
+++ src/api/controllers/authentication/registrationController.ts
@@ -1,31 +1,25 @@
-import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import handleRequest from '../../../utils/handlers/asyncHandler';
+import { checkIfEmailUnique } from '../../../utils/helpers/databaseHelpers';
 import { User, validate } from '../../models/user';
+import jwt from 'jsonwebtoken';
 
 const registrationController = {
-	async register(req: Request, res: Response) {
+	register: handleRequest(async (req, res) => {
 		const { error } = validate(req.body);
 		if (error) {
 			return res.status(400).json({ message: error.details[0].message });
 		}
 
-		let user = await User.findOne({ email: req.body.email });
-		if (user) {
-			return res.status(400).json({ message: 'User already registered.' });
-		}
+		await checkIfEmailUnique(User, req.body.email);
 
-		user = new User({
-			firstName: req.body.firstName,
-			lastName: req.body.lastName,
-			email: req.body.email,
-			password: req.body.password
-		});
+		const user = new User(req.body);
 
 		await user.save();
 
-		const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
-		res.header('Authorization', token).json({ message: 'User registered successfully.' });
-	}
+		const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET!, { expiresIn: '1d' });
+
+		res.header('Authorization', token).json({ message: 'User registered successfully.', token });
+	})
 };
 
 export default registrationController;
