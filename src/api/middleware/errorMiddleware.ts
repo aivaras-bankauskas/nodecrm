b292b5822ec4116c59b01bc7eefd839864000db5
aivaras <aivarasbankauskas@gmail.com>
2023-11-20 14:57:24 +0200

    errorMiddleware: optimize code

--- src/api/middleware/errorMiddleware.ts
+++ src/api/middleware/errorMiddleware.ts
@@ -2,18 +2,12 @@ import { Request, Response } from 'express';
 import CustomError from '../../utils/errors/customError';
 
 const errorMiddleware = (err: Error, _req: Request, res: Response) => {
-	let statusCode = 500;
-	let message = 'Internal Server Error';
-
 	if (err instanceof CustomError) {
-		statusCode = err.statusCode;
-		message = err.message;
+
+		return res.status(err.statusCode).json({ message: err.message });
 	}
 
-	res.status(statusCode).json({
-		status: 'error',
-		message
-	});
+	return res.status(500).json({ message: 'Internal server Error' });
 };
 
 export default errorMiddleware;
