21ab754d56e0962df059ebd2d0056f60489aa655
aivaras <aivarasbankauskas@gmail.com>
2023-11-20 16:01:23 +0200

    loginController.ts: update error handling

--- src/api/controllers/authentication/loginController.ts
+++ src/api/controllers/authentication/loginController.ts
@@ -1,28 +1,33 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import User from '../../models/user';
+import CustomError from '../../../utils/errors/customError';
 
 const loginController = {
-	async login(req: Request, res: Response) {
-		const user = await User.findOne({ email: req.body.email });
-		if (!user) {
-			return res.status(401).json({ message: 'Invalid email or password.' });
-		}
-
-		const validPassword = await bcrypt.compare(req.body.password, user.password);
-		if (!validPassword) {
-			return res.status(401).json({ message: 'Invalid email or password.' });
-		}
-
-		const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET!, { expiresIn: '1d' });
-		res.json(
-			{
-				message: 'User logged in successfully.',
-				data: user,
-				token
+	async login(req: Request, res: Response, next: NextFunction) {
+		try {
+			const user = await User.findOne({ email: req.body.email });
+			if (!user) {
+				throw new CustomError('Invalid email or password.', 401);
 			}
-		);
+
+			const validPassword = await bcrypt.compare(req.body.password, user.password);
+			if (!validPassword) {
+				throw new CustomError('Invalid email or password.', 401);
+			}
+
+			const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET!, { expiresIn: '1d' });
+			res.json(
+				{
+					message: 'User logged in successfully.',
+					data: user,
+					token
+				}
+			);
+		} catch (error) {
+			next(error);
+		}
 	}
 };
 
