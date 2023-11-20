2cab3fcb53f467aeb5547cc5200280ab3025b0a2
aivaras <aivarasbankauskas@gmail.com>
2023-11-20 15:49:54 +0200

    errorMiddleware.ts: update function

--- src/api/middleware/errorMiddleware.ts
+++ src/api/middleware/errorMiddleware.ts
@@ -4,10 +4,10 @@ import CustomError from '../../utils/errors/customError';
 const errorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
 	if (err instanceof CustomError) {
 
-		return res.status(err.statusCode).json({ message: err.message });
+		return res.status(err.statusCode).json({ errorMessage: err.message });
 	}
 
-	return res.status(500).json({ message: 'Internal server Error' });
+	return res.status(500).json({ errorMessage: 'Internal server Error' });
 };
 
 export default errorMiddleware;
